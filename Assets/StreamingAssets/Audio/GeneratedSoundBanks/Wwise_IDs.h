0754671ea868ee500e4085526902eb03b212eab6
nicolas <nico.valadou@orange.fr>
2021-12-06 17:25:40 +0100

    gitignore V2

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,62 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID LASER_CONSTANT = 2756076025U;
+        static const AkUniqueID LASER_END = 2510116948U;
+        static const AkUniqueID LASER_START_CHARGE = 4154444698U;
+        static const AkUniqueID LASER_START_RELEASE = 3217810413U;
+        static const AkUniqueID LASER_START_TRIGGER = 3428356050U;
+        static const AkUniqueID PLAY_CONSTANTLASER = 2735923927U;
+        static const AkUniqueID PLAYTEST = 96444927U;
+    } // namespace EVENTS
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID GP_MASTER = 2359434321U;
+        static const AkUniqueID GP_MUSIC = 687458188U;
+        static const AkUniqueID GP_SFX = 2681064004U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN_SOUNDBANK = 2228651116U;
+        static const AkUniqueID S_BALL = 1454594208U;
+        static const AkUniqueID S_BOSS = 1571743440U;
+        static const AkUniqueID S_CHARACTERS = 51703143U;
+        static const AkUniqueID S_MINIONS = 714890364U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID M_MAIN = 1683454542U;
+        static const AkUniqueID MASTER = 4056684167U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID SFX = 393239870U;
+        static const AkUniqueID SFX_BALL = 1639431926U;
+        static const AkUniqueID SFX_BOSS = 1421616930U;
+        static const AkUniqueID SFX_CHARACTER = 1415773282U;
+        static const AkUniqueID SFX_MINIONS = 4075503242U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
