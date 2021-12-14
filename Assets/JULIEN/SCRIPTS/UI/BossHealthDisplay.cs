d173c76eabee205545167e0d6d4dbe55a3f84155
Julien <julien.da_silva_soares@edu.devinci.fr>
2021-12-14 15:32:17 +0100

    Boss HP Bar + Shake player icon on hit

---
+++ Assets/JULIEN/SCRIPTS/UI/BossHealthDisplay.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossHealthDisplay : MonoBehaviour
+{
+    [SerializeField] private float maxHP;
+    [SerializeField] private float currentHP;
+
+    [SerializeField] private GameObject healthBar;
+
+    private RectTransform _rectTransformBar;
+    private float previousHP = float.MinValue;
+    private void Start()
+    {
+        _rectTransformBar = (RectTransform) healthBar.transform;
+    }
+
+    private void Update()
+    {
+        if (previousHP != currentHP)
+        {
+            _rectTransformBar.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, ((525f * currentHP) / maxHP));
+            previousHP = currentHP;
+        }
+    }
+
+    public void SetMaxHealth(float _maxHealth)
+    {
+        maxHP = _maxHealth;
+    }
+
+    public void SetCurrentHealth(float _currentHealth)
+    {
+        currentHP = _currentHealth;
+    }
+}
