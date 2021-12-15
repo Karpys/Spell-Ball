72c6ca4f363cec2d09c735b5abf8452d761bb10b
Julien <julien.da_silva_soares@edu.devinci.fr>
2021-12-15 16:46:04 +0100

    [Fix] Ball ralentie pas quand il y a un shield

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -17,7 +17,7 @@ public class Ball : MonoBehaviour
 
     public bool hitShield = false;
 
-    public float DelayDamageSelf;//Used for prevent self Damage //
+    public float DelayDamageSelf; //Used for prevent self Damage //
     private float DelayDamageSelfPlayer; // Same but for the Player//
 
     public bool Returnable;
@@ -59,8 +59,8 @@ public class Ball : MonoBehaviour
         RaycastHit hit;
 
         Vector3 forward = transform.forward * Speed * Time.deltaTime;
-        Debug.DrawRay(transform.position,forward, Color.green);
-        if (Physics.SphereCast(ray, radius, out hit, Speed * Time.deltaTime + radius,CollisionMask))
+        Debug.DrawRay(transform.position, forward, Color.green);
+        if (Physics.SphereCast(ray, radius, out hit, Speed * Time.deltaTime + radius, CollisionMask))
         {
             //TEST A SUPPRIMER//
             if (DestroyOnHitWall)
@@ -71,6 +71,7 @@ public class Ball : MonoBehaviour
             {
                 playBounce.Post(gameObject);
             }
+
             AddEffect();
             Reflect(hit.normal);
             Instantiate(CollisionParticle, transform.position, transform.rotation);
@@ -81,7 +82,8 @@ public class Ball : MonoBehaviour
             Reflect(hit.normal);
             //ResetSpeedAndCombo();
             hitShield = true;
-            Debug.Log(hit.transform.gameObject.GetComponent<Sheild>().color + "   " + gameObject.GetComponent<Balle>().color);
+            Debug.Log(hit.transform.gameObject.GetComponent<Sheild>().color + "   " +
+                      gameObject.GetComponent<Balle>().color);
             if (hit.transform.gameObject.GetComponent<Sheild>().color == gameObject.GetComponent<Balle>().color)
             {
                 Debug.Log("change");
@@ -93,10 +95,10 @@ public class Ball : MonoBehaviour
             gameObject.GetComponent<Balle>().ColorBallReset();
         }
 
-        
-        if (Physics.Raycast(ray, out hit, Time.deltaTime * Speed, CollisionMaskEnnemy) && DelayDamageSelf<0 && !hitShield)
+
+        if (Physics.Raycast(ray, out hit, Time.deltaTime * Speed, CollisionMaskEnnemy) && DelayDamageSelf < 0 &&
+            !hitShield)
         {
-            
             Reflect(hit.normal);
             Destroy(gameObject);
             if (hit.transform.gameObject && hit.transform.gameObject.TryGetComponent(out Manager_Life test))
@@ -105,20 +107,20 @@ public class Ball : MonoBehaviour
                 Manager_Life managerLife = hit.transform.gameObject.GetComponent<Manager_Life>();
                 managerLife.Damage(balle);
                 Instantiate(OnHitParticle, transform.position, transform.rotation);
-                managerLife.SummonHitParticle(transform.position, hit.transform.rotation, Color.red);//balle.trail.startColor);
-                
+                managerLife.SummonHitParticle(transform.position, hit.transform.rotation,
+                    Color.red); //balle.trail.startColor);
             }
-            ResetSpeedAndCombo();
 
+            ResetSpeedAndCombo();
         }
 
 
-
-        if (Physics.SphereCast(ray, radius, out hit, Speed * Time.deltaTime + radius, CollisionPlayer) && DelayDamageSelfPlayer < 0)
+        if (Physics.SphereCast(ray, radius, out hit, Speed * Time.deltaTime + radius, CollisionPlayer) &&
+            DelayDamageSelfPlayer < 0)
         {
             /*Reflect(hit.normal);*/
             /*ResetSpeedAndCombo();*/
-            
+
             Manager_Life Life = hit.collider.gameObject.GetComponentInParent<Manager_Life>();
             if (Life.Timerinvis <= 0)
             {
@@ -126,69 +128,70 @@ public class Ball : MonoBehaviour
                 Balle balle = GetComponent<Balle>();
                 if (Life.Timerinvis >= 0)
                 {
-
                 }
                 else
                 {
-                Life.DamageHealth(1);
-                Destroy(gameObject);
+                    Life.DamageHealth(1);
+                    Destroy(gameObject);
                 }
-
-
             }
         }
 
         if (Physics.Raycast(ray, out hit, Speed * Time.deltaTime * 5, CollisionMaskEnnemy))
         {
             BossBehavior bossBehavior = hit.collider.gameObject.GetComponent<BossBehavior>();
-            if (bossBehavior.ActualPhase == bossBehavior.Phases.Count - 2)
+            if (bossBehavior.transform.GetComponentInChildren<Sheild>() == null)
             {
-                int nextHP = (int) bossBehavior.Life.GetCurentLife() - gameObject.GetComponent<Balle>().combo;
-                if (nextHP <= 0)
+                if (bossBehavior.ActualPhase == bossBehavior.Phases.Count - 2)
                 {
-                    Time.timeScale = .025f;
-                    Camera.main.fieldOfView = 90;
-
-                    BossHpManager bossHpManager = hit.collider.GetComponent<BossHpManager>();
-
-                    Plane rightLeftPlane = new Plane(bossHpManager.transform.position, bossHpManager.inFrontOfBoss.position,
-                        bossHpManager.bottomOfBoss.position);
-                    Plane backFrontPlane = new Plane(bossHpManager.transform.position,
-                        bossHpManager.bottomOfBoss.position, bossHpManager.leftOfBoss.position);
-
-                    if (rightLeftPlane.GetSide(transform.position))
+                    int nextHP = (int) bossBehavior.Life.GetCurentLife() - gameObject.GetComponent<Balle>().combo;
+                    if (nextHP <= 0)
                     {
-                        // ON EST A GAUCHE DU BOSS
-                        if (backFrontPlane.GetSide(transform.position))
+                        Time.timeScale = .025f;
+                        Camera.main.fieldOfView = 90;
+
+                        BossHpManager bossHpManager = hit.collider.GetComponent<BossHpManager>();
+
+                        Plane rightLeftPlane = new Plane(bossHpManager.transform.position,
+                            bossHpManager.inFrontOfBoss.position,
+                            bossHpManager.bottomOfBoss.position);
+                        Plane backFrontPlane = new Plane(bossHpManager.transform.position,
+                            bossHpManager.bottomOfBoss.position, bossHpManager.leftOfBoss.position);
+
+                        if (rightLeftPlane.GetSide(transform.position))
                         {
-                            // ON EST DEVANT LE BOSS
-                            Camera.main.transform.parent.position = bossHpManager.leftFrontCameraSpot.position;
+                            // ON EST A GAUCHE DU BOSS
+                            if (backFrontPlane.GetSide(transform.position))
+                            {
+                                // ON EST DEVANT LE BOSS
+                                Camera.main.transform.parent.position = bossHpManager.leftFrontCameraSpot.position;
+                            }
+                            else
+                            {
+                                Camera.main.transform.parent.position = bossHpManager.leftBackCameraSpot.position;
+                            }
                         }
                         else
                         {
-                            Camera.main.transform.parent.position = bossHpManager.leftBackCameraSpot.position;
+                            if (backFrontPlane.GetSide(transform.position))
+                            {
+                                Camera.main.transform.parent.position = bossHpManager.rightFrontCameraSpot.position;
+                            }
+                            else
+                            {
+                                Camera.main.transform.parent.position = bossHpManager.rightBackCameraSpot.position;
+                            }
                         }
-                    }
-                    else
-                    {
-                        if (backFrontPlane.GetSide(transform.position))
-                        {
-                            Camera.main.transform.parent.position = bossHpManager.rightFrontCameraSpot.position;
-                        }
-                        else
-                        {
-                            Camera.main.transform.parent.position = bossHpManager.rightBackCameraSpot.position;
-                        }
-                    }
 
-                    centerOfAction = ((transform.position + hit.collider.transform.position) / 2);
-                    
-                    InvokeRepeating("LookAtAction", 0, 0.001f);
+                        centerOfAction = ((transform.position + hit.collider.transform.position) / 2);
+
+                        InvokeRepeating("LookAtAction", 0, 0.001f);
+                    }
                 }
             }
         }
-        transform.Translate(Vector3.forward * Time.deltaTime * Speed);
 
+        transform.Translate(Vector3.forward * Time.deltaTime * Speed);
     }
 
     public void LookAtAction()
@@ -198,10 +201,11 @@ public class Ball : MonoBehaviour
 
     public void Reflect(Vector3 Normal)
     {
-        Vector3 Reflect = Vector3.Reflect(transform.forward,Normal);
+        Vector3 Reflect = Vector3.Reflect(transform.forward, Normal);
         float NewRotation = Mathf.Atan2(Reflect.x, Reflect.z) * Mathf.Rad2Deg;
         transform.eulerAngles = new Vector3(0, NewRotation, 0);
     }
+
     public void SetDirection(Vector3 EulerAngles)
     {
         transform.eulerAngles = EulerAngles;
@@ -230,12 +234,14 @@ public class Ball : MonoBehaviour
 
     public void LookAtStart(GameObject Target)
     {
-        Vector3 _direction = (new Vector3(Target.transform.position.x,transform.position.y,Target.transform.position.z) - transform.position).normalized;
+        Vector3 _direction =
+            (new Vector3(Target.transform.position.x, transform.position.y, Target.transform.position.z) -
+             transform.position).normalized;
 
         //create the rotation we need to be in to look at the target
         Quaternion _lookRotation = Quaternion.LookRotation(_direction);
 
         //rotate us over time according to speed until we are in the required rotation
-        transform.rotation = Quaternion.Slerp(transform.rotation, _lookRotation,1);
+        transform.rotation = Quaternion.Slerp(transform.rotation, _lookRotation, 1);
     }
 }
